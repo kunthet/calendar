df5500af97ea2ec4bad6d519b60db71164778ef1
Sokunthet Sok <kunthet@users.noreply.github.com>
2023-04-02 14:03:54 +0700

    auto commit @Sun Apr 02 2023 14:03:54 GMT+0700 (Indochina Time)

--- commands/commands.js
+++ commands/commands.js
@@ -2,9 +2,23 @@
 // eslint-disable-next-line no-undef
 const g = getGlobal();
 
-function addToday() {
+async function addToday(args) {
     console.log('hi from command.');
     console.log('getGlobal: ', g);
+
+    try {
+        await window.Excel.run(async (context) => {
+            const range = context.workbook.getSelectedRange();
+            range.format.fill.color = "yellow";
+            await context.sync();
+        });
+    } catch (error) {
+        // Note: In a production add-in, notify the user through your add-in's UI.
+        console.error(error);
+    }
+
+    // Calling event.completed is required. event.completed lets the platform know that processing has completed.
+    args.completed();
 }
 
 
