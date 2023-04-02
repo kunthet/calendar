6445f6a6851707254cbef86dbb5a903dc256824c
Sokunthet Sok <kunthet@users.noreply.github.com>
2023-04-03 00:02:55 +0700

    auto commit @Mon Apr 03 2023 00:02:55 GMT+0700 (Indochina Time)

---
+++ js/574.e0858868.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkhmer_date_word_addins"]=self["webpackChunkkhmer_date_word_addins"]||[]).push([[574],{6574:function(n,r,e){e.r(r),e.d(r,{default:function(){return s}});var u=e(3396);const d=(0,u._)("h1",null,"Privacy policy",-1),t=(0,u._)("p",null,"coming soon...",-1),a=[d,t];function c(n,r){return(0,u.wg)(),(0,u.iD)("div",null,a)}var i=e(89);const o={},l=(0,i.Z)(o,[["render",c]]);var s=l}}]);
+//# sourceMappingURL=574.e0858868.js.map
